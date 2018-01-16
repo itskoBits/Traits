12a9b77c1f650d9eda53913b83f4eb997c43e1f7
Ivelin Davidov <ivelin.davidov@tumba.solutions>
2018-01-16 12:21:35 +0200

    Added support for Carthage.

---
+++ Framework/Traits.h
@@ -0,0 +1,19 @@
+//
+//  Traits.h
+//  Traits
+//
+//  Created by Ivelin Davidov on 1/16/18.
+//  Copyright © 2018 itskoBits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Traits.
+FOUNDATION_EXPORT double TraitsVersionNumber;
+
+//! Project version string for Traits.
+FOUNDATION_EXPORT const unsigned char TraitsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Traits/PublicHeader.h>
+
+
